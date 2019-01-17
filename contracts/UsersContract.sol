7021f2f63b3c9a4239570ba07a51d8fdc796e112
JorTurFer <jorge_turrado@hotmail.es>
2019-01-17 23:53:08 +0100

    Añado funcion de seguridad

--- contracts/UsersContract.sol
+++ contracts/UsersContract.sol
@@ -4,20 +4,25 @@ contract UsersContract {
     
     struct User{
         string name;
-        string surname;
+        string surName;
     }
 
     mapping(address => User) private users;
+    mapping(address => bool) private joinedUsers;
 
-    function join(string name, string surname) public{
+    function join(string name, string surName) public {
         User storage user = users[msg.sender];
         user.name = name;
-        user.surname = surname;
+        user.surName = surName;
+        joinedUsers[msg.sender] = true;
     }
 
     function getUser(address addr) public view returns (string,string){
         User memory user = users[addr];
-        return (user.name,user.surname);
+        return (user.name,user.surName);
     }
 
+    function userJoined (address addr) private view returns (bool){
+        return joinedUsers[addr];
+    }
 }
