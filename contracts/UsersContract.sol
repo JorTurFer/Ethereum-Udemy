84e4a7e870fee95363c4d3263d541105d7d6f50a
JorTurFer <jorge_turrado@hotmail.es>
2019-01-17 23:54:47 +0100

    Añado check para ver si existe o no el usuario

--- contracts/UsersContract.sol
+++ contracts/UsersContract.sol
@@ -10,7 +10,10 @@ contract UsersContract {
     mapping(address => User) private users;
     mapping(address => bool) private joinedUsers;
 
+
+
     function join(string name, string surName) public {
+        require(!joinedUsers[msg.sender]);
         User storage user = users[msg.sender];
         user.name = name;
         user.surName = surName;
@@ -18,6 +21,7 @@ contract UsersContract {
     }
 
     function getUser(address addr) public view returns (string,string){
+        require(joinedUsers[msg.sender]);
         User memory user = users[addr];
         return (user.name,user.surName);
     }
